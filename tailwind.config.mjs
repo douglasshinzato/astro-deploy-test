77404ffc225c419c6b5a81f4b081ea5e45f2c136
Douglas Shinzato <shinzato.douglas@gmail.com>
2024-04-20 20:48:51 -0400

    deploy test

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Roboto: "'Roboto', sans-serif",
+        Inter: "'Inter', sans-serif",
+      },
+      colors: {
+        dark: {
+          100: "#191622",
+        },
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
 }
